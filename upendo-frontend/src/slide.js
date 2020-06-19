2b4a1107527f03cf8b5a5a56e577b584d90b2210
trinhcon <conroy.trinh@mail.utoronto.ca>
2020-06-19 02:10:22 -0400

    frontend initialization, plus some beekeeper profile starter code for Beekeeper Flow

---
+++ upendo-frontend/src/slide.js
@@ -0,0 +1,18 @@
+class Slide extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  
+  previousPage() {
+    /* Route to previous Page */
+  }
+
+  nextPage() {
+    /* Route to next page*/
+  }
+
+  render() {
+
+  }
+}
