e164dcebac5e6acc644aac54a7320c692ae1a980
trinhcon <conroy.trinh@mail.utoronto.ca>
2020-06-20 19:57:37 -0400

    Implemented Mobile Appearance of Beekeeper Profile and Headers & Footers for flows

--- upendo-frontend/src/App.js
+++ upendo-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import LandingPage from "./landing-page";
 import BeekeeperContainer from "./flows/beekeeper-flow/beekeeper-portrait";
 
+
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./landing-page.css";
 
@@ -10,7 +11,14 @@ class App extends React.Component {
     return (
       <Router>
         <Switch>
-          <Route path = "/beekeeper" component = {BeekeeperContainer} />
+          <Route path = "/beekeeper" render={() =>   (
+            <BeekeeperContainer imageURL="https://www.petakids.com/wp-content/uploads/2015/11/Cute-Red-Bunny.jpg"
+              beekeeperDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut erat tincidunt, lobortis nisi sit amet, ullamcorper neque. Vivamus iaculis ac enim et laoreet. Phasellus aliquam porta vehicula. Maecenas porta. "
+              beekeeperName="Picu, Rat's new best friend"
+            />
+            )}
+
+          />
           <Route path = "/" component = {LandingPage} />
         </Switch>
       </Router>
