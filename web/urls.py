d56b108f6c448c3831276d84b8d528b6a3959dea
Ashwin Balamohan <abmohan@gmail.com>
2020-06-21 14:25:49 -0400

    Standardized admin and API to match models

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from web import views
 
-from . import views
+
+router = routers.DefaultRouter()
+router.register(r'batches', views.BatchViewSet)
+router.register(r'batch-members', views.BatchMemberViewSet)
+router.register(r'beekeepers', views.BeekeeperViewSet)
+router.register(r'forests', views.ForestViewSet)
+router.register(r'honey', views.HoneyViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/v1/batches/<int:batch_id>', views.batch_detail, name='batch_detail'),
-    path('api/v1/batch-member/<int:alphanum_code>', views.get_batch_member, name='get_batch_member'),
-    path('api/v1/beekeeper/<int:beekeeper_id>', views.get_beekeeper, name='get_beekeeper'),
+    path('api/v1/', include(router.urls)),
 ]
