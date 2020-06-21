856e18f6439b9669ad74f1f0c756c96bfc91213c
Linda <adnil3910@gmail.com>
2020-06-21 19:43:19 -0400

    Added routing to new pages

--- upendo-frontend/src/App.js
+++ upendo-frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import LandingPage from "./landing-page";
-import BeekeeperContainer from "./flows/beekeeper-flow/beekeeper-portrait";
+import BeekeeperPage from "./flows/beekeeper-flow/beekeeper-portrait";
+import BeekeeperMessagePage from "./flows/beekeeper-flow/beekeeper-message";
+import BeekeeperLetterPage from "./flows/beekeeper-flow/beekeeper-letter";
+import MenuPage from "./flows/menu";
 
 
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
@@ -11,8 +14,22 @@ class App extends React.Component {
     return (
       <Router>
         <Switch>
+          <Route path = "/menu" render = {() => (
+            <MenuPage beekeeperFirstPage="/beekeeper"
+              environmentFirstPage="/blah1"
+              honeyFirstPage="/blah2"
+              tanzaniaFirstPage="/blah3"
+            /> 
+            )}
+          />
+          <Route path = "/beekeeper-letter" render = {() => (
+              <BeekeeperLetterPage bk = {{letter: "https://www.petakids.com/wp-content/uploads/2015/11/Cute-Red-Bunny.jpg", translation: "blah"}}
+              />
+            )}
+          />
+          <Route path = "/beekeeper-message" component = {BeekeeperMessagePage}/>
           <Route path = "/beekeeper" render={() =>   (
-            <BeekeeperContainer imageURL="https://www.petakids.com/wp-content/uploads/2015/11/Cute-Red-Bunny.jpg"
+            <BeekeeperPage imageURL="https://www.petakids.com/wp-content/uploads/2015/11/Cute-Red-Bunny.jpg"
               beekeeperDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ut erat tincidunt, lobortis nisi sit amet, ullamcorper neque. Vivamus iaculis ac enim et laoreet. Phasellus aliquam porta vehicula. Maecenas porta. "
               beekeeperName="Picu, Rat's new best friend"
             />
