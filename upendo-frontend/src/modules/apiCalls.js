837117f1155d6f50cfeaf029d8b0ece2aae5ad38
Linda <adnil3910@gmail.com>
2020-06-22 15:12:07 -0400

    Created and tested modules for api fetching.

---
+++ upendo-frontend/src/modules/apiCalls.js
@@ -0,0 +1,27 @@
+export async function retrieveBatchMember(code) {
+    let batchMemberData = null;
+    const url = "http://127.0.0.1:8000/api/v1/batch-members/";
+    const options = {mode:'no-cors'};
+
+    // Not checking for errors yet
+    const batchMemberResp = await fetch(url + code, options);
+    console.log(batchMemberResp);
+    console.log(batchMemberResp.status);
+    if (batchMemberResp.ok) {
+        batchMemberData = batchMemberResp.json();
+    } 
+
+    return batchMemberData;
+}
+
+export async function retrieveBeekeeper(id) {
+    let beekeeperData = null;
+    const url = "http://127.0.0.1:8000/api/v1/beekeepers/"
+    const options = {mode: 'no-cors'};
+
+    const beekeeperResp = await fetch(url + id, options);
+    if (beekeeperResp.ok) {
+        beekeeperData = beekeeperResp.json();
+    }
+    return beekeeperData;
+}
